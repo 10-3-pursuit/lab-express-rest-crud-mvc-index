f8840c802183ff7f9459e9554d841f609caf9e3e
Haiyah Perez <haiyahperez@pursuit.org>
2024-02-22 15:09:43 -0500

    start

---
+++ app.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+const express = require('express')
+
+
+// CONFIGURATION
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+// EXPORT
+module.exports = app
