33d784d772e94c1fc49d1fede8c33d54996324b0
juligarc91 <julissagarcia@pursuit.org>
2024-02-22 17:00:48 -0500

    created files and added route for location

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 // Config
 const app = express();
 
+// locations
+const locationsController = require('./controllers/locations.controller.js') // importing
+app.use('/locations', locationsController) // mounting controller to path
+
 // Routes
 app.get('/',(req, res) => {
     res.json({'message':'Hello, world!'});
---
+++ notes.md
@@ -0,0 +1,22 @@
+http://localhost:3333/locations in browser should display:
+
+```json
+{
+"locations": [
+{
+"id": 1,
+"street": "45 Davis Street",
+"city": "Long Island City",
+"state": "NY",
+"zip": "11101"
+},
+{
+"id": 2,
+"street": "23 Broadway",
+"city": "New York",
+"state": "NY",
+"zip": "11011"
+}
+]
+}
+```
