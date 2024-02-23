2936c757c3703cc980e6ab2396d0ed6853b20e48
juligarc91 <julissagarcia@pursuit.org>
2024-02-22 21:04:51 -0500

    created path with locations and persons models together based on zip code and main location

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const locations = express.Router(); // kind of like browser router, routes, and routing in react.js
-const locationsArr = require("../models/locations.model");
+const locationsArr = require('../models/locations.model');
+const personsArr = require('../models/persons.model')
 
 locations.get('/', (req, res) => {
     res
@@ -8,6 +9,19 @@ locations.get('/', (req, res) => {
     .json({ locations: locationsArr })
 });
 
-
+/* --- BONUS ---
+need to make path /persons in this file so it can be locations/persons
+need to get data from location and transform it (.map because .map can change data type or structure)
+"The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array."
+then add persons array of objects?
+*/
+locations.get('/persons', (req, res) => {
+    // for every zipcode value in locations array of obj:
+    // the data must have a persons array
+    // the persons array must have persons objects
+    // the persons objects contains mainLocation key where value matches zipcode value in locations array of obj
+    let result = locationsArr.map(locationObj => locationObj); // need to add persons array to each object
+    res.json(result)
+})
 
 module.exports = locations;
