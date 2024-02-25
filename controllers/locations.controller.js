bf9cc4bdcf4e3f1112a1089263337cce915e8a5e
juligarc91 <julissagarcia@pursuit.org>
2024-02-25 04:53:35 -0500

    finished bonus and added comments

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -9,22 +9,16 @@ locations.get('/', (req, res) => {
     .json({ locations: locationsArr })
 });
 
-/* --- BONUS ---
-need to make path /persons in this file so it can be locations/persons
-need to get data from location and transform it (.map because .map can change data type or structure)
-"The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array."
-then add persons array of objects
-*/
+// --- BONUS ---
 locations.get('/persons', (req, res) => {
     // for every zipcode value in locations array of obj:
     // the data must have a persons array
     // the persons array must have persons objects
     // the persons objects contains mainLocation key where value matches zipcode value in locations array of obj
     let result = locationsArr.map(locationObj => {
-        let matchingPersons = personsArr.filter(persons => persons.mainLocation === locationObj.zip)
-        return {...locationObj, persons: matchingPersons}
-        //spread op to not mutate original
-    }); // need to add persons array to each object based on zipcode and main location keys and values so .filter
+        let matchingPersons = personsArr.filter(persons => persons.mainLocation === locationObj.zip) // need to add persons array to each object based on zipcode and main location keys and values so .filter
+        return {...locationObj, persons: matchingPersons}//spread op to not mutate original
+    }); 
     result.length >= 0 ? res.status(200).json({personLocation: result}) : res.status(400).json({message: "error!"})
 })
 
